505dcf672017bbdefc542845cc92fdf8269a0f52
Sreyeso <sreyeso@unal.edu.co>
2022-05-14 22:14:23 -0500

    Multiverso Basico

--- Multiverso.js
+++ Multiverso.js
@@ -1,40 +1,78 @@
-
-class Pila extends ListaEnlazada{
-
-    // #Herencia:
-    // #__init__
-    // #PushFront
-    // #Empty
-    // #Print
-    // #Find
-    // #retN
-    // #Erase
-
-    TopFront(){
-        if (this.head.data!=null)
-        {
-            return this.head.data;
-        }
+class Universo{
+    constructor(data,x,y,r){
+        this.data=data;
+        this.x=x;
+        this.y=y;
+        this.r=r;
+        this.conexiones=[];
     }
 
-    PopFront(){
-        if (this.tail!=this.head)
-        {
-            dato = this.head.data;
-            this.head = this.head.next
-            this.size -= 1
-            return dato
-        }
-        else
-        {
-            if(self.size>0)
-            {
-                this.head=null;
-                this.tail=null;
-                self.size-=1;
-                return dato;
+    Print(){
+        push();
+        textSize(15);
+        fill('white');
+        stroke('black');
+        strokeWeight(1);
+        circle(this.x,this.y,this.r);
+        fill('black');
+        text(this.data,this.x,this.y);
+        pop();
+    }
+
+}
+
+
+class Multiverso{
+
+    //Solo es posible viajar a multiversos cercanos 
+    //Puede conectarse con maximo 6 multiversos
+    //Solo es posible viajar en una direccion (ida)
+
+    constructor(max_conexiones){
+        this.independientes=[];
+        this.size=0;
+        this.max=max_conexiones;
+    }
+
+    CrearUniversoIndependiente(data,x,y,r){
+        this.independientes.push(new Universo(data,x,y,r));
+        this.size++;
+    }
+
+    CrearUniversoDependiente(conexiones,data,x,y,r){
+        let uni = new Universo(data,x,y,r);
+        for(let i=0; i<conexiones.length;i++){
+            if(conexiones[i].conexiones.length<6){
+                conexiones[i].conexiones.push(uni);
             }
-            return;
+            else{
+                throw "Maximo de conexiones permitidas = 6";
+            }
         }
     }
+
+    Print(){
+        push();
+        for(let i=0; i<this.independientes.length;i++){
+            //Imprimir conexiones
+            for(let j=0; j<this.independientes[i].conexiones.length;j++){
+                line(   this.independientes[i].x,
+                        this.independientes[i].y,
+                        this.independientes[i].conexiones[j].x,
+                        this.independientes[i].conexiones[j].y);
+                this.independientes[i].conexiones[j].Print();
+            }
+            //Imprimir nodos
+            textSize(15);
+            fill('white');
+            stroke('black');
+            strokeWeight(1);
+            circle(this.independientes[i].x,this.independientes[i].y,this.independientes[i].r);
+            fill('black');
+            text(this.independientes[i].data,this.independientes[i].x-(0.5*15),this.independientes[i].y+(0.4*15));
+        }
+        pop();
+    }
+
+
 }
--- test.js
+++ test.js
@@ -1,5 +1,4 @@
 let test;
-
 function setup() {
     createCanvas(windowWidth, windowHeight);
     background('tomato');
@@ -8,8 +7,16 @@ function setup() {
     // test.PushFront(3,150,300,100);
     // test.PushFront(69,700,536,50);
     // test.PushBack(15,900,200,70);
-    test = new Pila();
-    test.PushFront(12,15,15,30);
+    test = new Multiverso();
+    test.CrearUniversoIndependiente(20,windowWidth/2,windowHeight/2,50);
+    test.CrearUniversoDependiente([test.independientes[0]],1,random(70,windowWidth-70),random(70,windowHeight-70),random(30,70));
+    test.CrearUniversoDependiente([test.independientes[0]],2,random(70,windowWidth-70),random(70,windowHeight-70),random(30,70));
+    test.CrearUniversoDependiente([test.independientes[0]],3,random(70,windowWidth-70),random(70,windowHeight-70),random(30,70));
+    test.CrearUniversoDependiente([test.independientes[0]],4,random(70,windowWidth-70),random(70,windowHeight-70),random(30,70));
+    test.CrearUniversoDependiente([test.independientes[0]],5,random(70,windowWidth-70),random(70,windowHeight-70),random(30,70));
+    test.CrearUniversoDependiente([test.independientes[0]],6,random(70,windowWidth-70),random(70,windowHeight-70),random(30,70));
+    //No permitido
+    //test.CrearUniversoDependiente([test.independientes[0]],7,random(windowWidth),random(windowHeight),random(30,70));
 }
 
 function draw() {
